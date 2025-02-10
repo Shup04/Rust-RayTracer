f3e1bab30b217b92e5b9c21bddb5c02273e7ce62
Shup04 <bradleyschmidt04@gmail.com>
2025-02-09 21:00:49 -0800

    image displaying done, worked on some Vec3 functions

---
+++ raytracer/src/vec3.rs
@@ -0,0 +1,60 @@
+use std::fmt::{Display, Formatter, Result};
+use std::ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Neg, Sub};
+
+#[derive(Copy, Clone, Default)]
+
+// Vector3 Basic Implementation
+// A struct with a 3 element array for x, y, z. 
+// Also can be used for color, R, G, b.
+// As well as 3 functions for getting x, y, z.
+// You can store a color in a location vector although it would be confusing.
+
+pub struct Vec3 {
+    e: [f64; 3],
+}
+
+impl Vec3 {
+    pub fn new(x: f64, y: f64, z: f64) -> Vec3 {
+        Vec3 { e: [x,y,z] }
+    }
+
+    pub fn x(&self) -> f64 {
+        self.e[0]
+    }
+
+    pub fn y(&self) -> f64 {
+        self.e[1]
+    }
+
+    pub fn z(&self) -> f64 {
+        self.e[2]
+    }
+
+    pub fn length(&self) -> f64 {
+        f64::sqrt(self.length_squared())
+    }
+
+    pub fn length_squared(&self) -> f64 {
+        self.e[0] * self.e[0] + self.e[1] * self.e[1] + self.e[2] * self.e[2]
+    }
+}
+
+// Create a public type 'Point3' that is identical to Vec3.
+// This is used for clarity since im using Vec3 for both color and location.
+pub type Point3 = Vec3;
+
+// Output Formatting
+impl Display for Vec3 {
+    fn fmt(&self, f: &mut Formatter) -> Result {
+        write!(f, "{} {} {}", self.e[0], self.e[1], self.e[2])
+    }
+}
+
+// -Vec3
+impl Neg for Vec3 {
+    type Output = Vec3;
+
+    fn neg(self) -> Vec3 {
+        Vec3::new(-self.x(), -self.y(), -self.z())
+    }
+}
