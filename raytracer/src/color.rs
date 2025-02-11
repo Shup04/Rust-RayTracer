aa1cd5a3b57fc52865451617cd125afe3750cb02
Shup04 <Bradleyschmidt04@gmail.com>
2025-02-10 18:31:19 -0800

    added skybox

--- raytracer/src/color.rs
+++ raytracer/src/color.rs
@@ -1,4 +1,4 @@
- use std::io::write;
+ use std::io::Write;
 
  use crate::vec3::Vec3;
 
@@ -7,8 +7,8 @@ pub type Color = Vec3;
 
 pub fn write_color(out: &mut impl Write, pixel_color: Color) {
     // Take the R B G of a Vec3 and convert it to the 255 value and print
-    let r = (255.999, pixel_color.x()) as i32;
-    let g = (255.999, pixel_color.y()) as i32;
-    let b = (255.999, pixel_color.y()) as i32;
+    let r = (255.999 * pixel_color.x()) as i32;
+    let g = (255.999 * pixel_color.y()) as i32;
+    let b = (255.999 * pixel_color.y()) as i32;
     writeln!(out, "{} {} {}", r, g, b).expect("writing color");
 }
