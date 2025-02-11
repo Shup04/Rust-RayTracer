827d1e1d346bf73f87e88dbc459dc9f997653bfd
Shup04 <bradleyschmidt04@gmail.com>
2025-02-10 20:53:19 -0800

    added sphere, hittable class, and hittable list for multiple hittable objects

--- raytracer/src/main.rs
+++ raytracer/src/main.rs
@@ -7,7 +7,26 @@ use color::Color;
 use ray::Ray;
 use vec3::{Point3, Vec3};
 
+fn hit_sphere(center: Point3, radius: f64, r: &Ray) -> f64 {
+    let oc = r.origin() - center;
+    let a = r.direction().length_squared();
+    let half_b = vec3::dot(oc, r.direction());
+    let c = oc.length_squared() - radius * radius;
+    let discriminant = half_b * half_b - a * c;
+    if discriminant < 0.0 {
+        -1.0
+    } else {
+        (-half_b - f64::sqrt(discriminant)) / a
+    }
+}
+
 fn ray_color(r: &Ray) -> Color {
+    let t = hit_sphere(Point3::new(0.0, 0.0, -1.0), 0.5, r);
+
+    if t > 0.0 {
+        let n = vec3::unit_vector(r.at(t) - Vec3::new(0.0, 0.0, -1.0));
+        return 0.5 * Color::new(n.x() + 1.0, n.y() + 1.0, n.z() + 1.0);
+    }
     let unit_direction = vec3::unit_vector(r.direction());
     let t = 0.5 * (unit_direction.y() + 1.0);
     (1.0 - t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(0.5, 0.7, 1.0)
@@ -16,8 +35,8 @@ fn ray_color(r: &Ray) -> Color {
 fn main() {
     //Image
     const ASPECT_RATIO: f64 = 16.0 / 9.0;
-    const IMAGE_WIDTH: i32 = 256;
-    const IMAGE_HEIGHT: i32 = 256;
+    const IMAGE_WIDTH: i32 = 400;
+    const IMAGE_HEIGHT: i32 = (IMAGE_WIDTH as f64 / ASPECT_RATIO) as i32;
 
     // Camera
     let viewport_height = 2.0;
